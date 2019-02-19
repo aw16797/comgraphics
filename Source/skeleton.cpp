6cef43e4d7afdd1d51e7c8aa7bd49860775edf5f
Jason Greenfield <33734332+jasonbgreenfield@users.noreply.github.com>
2019-02-19 10:42:39 +0000

    Messy version-working direct illumin, shadows, indirect illumin (no rotation)

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -11,10 +11,11 @@ using glm::vec3;
 using glm::mat3;
 using glm::vec4;
 using glm::mat4;
+using std::max;
 
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128
+#define SCREEN_WIDTH 555
+#define SCREEN_HEIGHT 555
 #define FULLSCREEN_MODE true
 
 /* ----------------------------------------------------------------------------*/
@@ -26,30 +27,33 @@ struct Intersection{
 };
 
 
-bool Update(vec4& cameraPos, float& yaw);
-void Draw(screen* screen, vec4 cameraPos, float yaw);
+bool Update(vec4& cameraPos, float& yaw, vec4& lightPos);
+void Draw(screen* screen, vec4 cameraPos, float yaw, vec4 lightPos);
 bool ClosestIntersection( vec4 start,
                           vec4 dir,
                           const vector<Triangle>& triangles,
                           Intersection& closestIntersection );
+vec3 DirectLight(Intersection interS, vec4 lightPos);
 float getFocalLength(vec4 camera);
 
 
 
 
-int main( int argc, char* argv[] )
-{
+int main( int argc, char* argv[] ){
 
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
-  vec4 cameraPos(0, 0, -1110/SCREEN_WIDTH, 1);
+  vec4 cameraPos(0, 0, -2.75, 1);
 
   float yaw = 0;
 
-  while( Update(cameraPos, yaw) ) //NoQuitMessageSDL() )
+  //this will be passe dinto update and then into DirectLight
+  vec4 lightPos( 0, -0.5, -0.7, 1.0 );
+
+  while( Update(cameraPos, yaw, lightPos) ) //NoQuitMessageSDL() )
     {
       // Update();
-      Draw(screen, cameraPos, yaw);
+      Draw(screen, cameraPos, yaw, lightPos);
       SDL_Renderframe(screen);
     }
 
@@ -98,12 +102,7 @@ float getFocalLength(vec4 camera){
   return 226.274;
 }
 
-
-
-bool ClosestIntersection( vec4 start,
-                          vec4 dir,
-                          const vector<Triangle>& triangles,
-                          Intersection& closestIntersection ){
+bool ClosestIntersection( vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection ){
 
   bool toReturn = false;
   float closestSoFar = std::numeric_limits<float>::max();
@@ -176,9 +175,50 @@ bool ClosestIntersection( vec4 start,
 
 }
 
+vec3 DirectLight(Intersection interS, vec4 lightPos){
+  vec3 toReturn(0, 0, 0);
+
+  vec3 lightColor = 14.f * vec3( 1, 1, 1 ); //original from directions
+  // vec3 lightColor = 14.f * vec3( .01, .01, .01); //looks better with this, but idk why (there's too much power (pixels are getting too much light) so all the pixels are white)
+
+  vector<Triangle> triangles;
+  LoadTestModel( triangles );
+
+  vec4 rCarrot(lightPos.x - interS.position.x,
+         lightPos.y - interS.position.y,
+         lightPos.z - interS.position.z,
+         // lightPos.w - interS.position.w); // this is what I was doing when it didn't really work...maybe this should always just be 1 still?
+         1);
+  vec4 nCarrot = triangles[interS.triangleIndex].normal;
+  float radius = sqrt( pow(lightPos.x - interS.position.x ,2) + pow(lightPos.y - interS.position.y, 2) + pow(lightPos.z - interS.position.z, 2) + pow(lightPos.w - interS.position.w, 2) ); //diong it with 1 for w now instead of subtracting them
+  // float radius = sqrt( pow(lightPos.x - interS.position.x ,2) + pow(lightPos.y - interS.position.y, 2) + pow(lightPos.z - interS.position.z, 2) + pow(lightPos.w - interS.position.w, 2) );
+  float dotProduct = (rCarrot.x*nCarrot.x + rCarrot.y*nCarrot.y + rCarrot.z*nCarrot.z + rCarrot.w*nCarrot.w );
+  if( dotProduct > 0){
+    float multiplyBy = ( dotProduct/ (4*pow(radius, 2)*M_PI) );
+    toReturn.x = lightColor.x*multiplyBy;
+    toReturn.y = lightColor.y*multiplyBy;
+    toReturn.z = lightColor.z*multiplyBy;
+  }
+
+  //adding shadows here:
+  //I check if the distance between interS and lightPos is >, <
+  // the distance between lightPos and closestIntersection
+
+  Intersection closestToLight;
+  vec4 shadowDirection(rCarrot.x*-1, rCarrot.y*-1, rCarrot.z*-1, 1);
+  if( ClosestIntersection(lightPos, shadowDirection, triangles, closestToLight) ){
+    float tempDistance = sqrt( pow(lightPos.x - closestToLight.position.x, 2) + pow(lightPos.y - closestToLight.position.y, 2) + pow(lightPos.z - closestToLight.position.z, 2) + 1  ); //this is distance from light to closest intersection
+    if(tempDistance < radius){
+      return vec3(0, 0, 0);
+    }
+  }
+
+  return toReturn;
+
+}
+
 /*Place your drawing here*/
-void Draw(screen* screen, vec4 cameraPos, float yaw)
-{
+void Draw(screen* screen, vec4 cameraPos, float yaw, vec4 lightPos){
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
 
@@ -206,17 +246,50 @@ void Draw(screen* screen, vec4 cameraPos, float yaw)
 
   // v/f = y/z, where f = focal length in pixels, z = distance for cameraPos, v = half of SCREEN_WIDTH, and y = half of model height
   //this is how we get focal length and z values for cameraPos
+
+  //start of getting helper variables for rotated riection 18 Feb
+  // float h = ( sqrt( pow(cameraPos.x, 2) + pow(cameraPos.z, 2) )*cos(yaw) );
+  float h = focalLength;
+  float dirX = ( h*cos(M_PI/2-yaw) );
+  float dirZ = ( h*sin(M_PI/2-yaw) );
+
+  // cout << "In draw, (dirX: "
+  //      << dirX << ","
+  //      << "dirZ: " << dirZ << ", "
+  //      << "yaw: " << yaw << ", "
+  //      << ")" << endl;
+  //end of helpers from 18 Feb rotated dir
+
   for(int i = 0; i < SCREEN_WIDTH; i ++){ // usually SCREEN_WIDTH
     for(int j = 0; j < SCREEN_HEIGHT; j++){ // usually SCREEN_HEIGHT
 
-      // cout << "In draw, (I: "
-      //      << i << ","
-      //      << "J: " << j << ", "
+      // cout << "In draw, (dirX: "
+      //      << dirX << ","
+      //      << "dirZ: " << dirZ << ", "
       //      << ")" << endl;
 
-      vec4 dir(i - (SCREEN_WIDTH/2), j - (SCREEN_HEIGHT/2), focalLength, 1);
+      //for the regular working picture
+      vec4 dir(i - (SCREEN_WIDTH/2), j - (SCREEN_HEIGHT/2), focalLength, 1); //this was how we did dir before starting the camera rotation on 18 Feb
+
+      //for trying to do rotation
+      // vec4 dir( dirX + i - (SCREEN_WIDTH/2), j - (SCREEN_HEIGHT/2), dirZ, 1);
+
       if( ClosestIntersection(cameraPos, dir, triangles, closest) ){
-        PutPixelSDL(screen, i, j, triangles[closest.triangleIndex].color );
+        //for regular colored no lighitng
+        // PutPixelSDL(screen, i, j, triangles[closest.triangleIndex].color ); //this is how we did it pre-illumination
+
+        //for fancy black and white shading
+        // PutPixelSDL(screen, i, j, DirectLight(closest, lightPos) ); // this is how we're diong it with B&W illumination
+
+        //for fancy in color shading (now with shadows too!)
+        // PutPixelSDL(screen, i, j, triangles[closest.triangleIndex].color * DirectLight(closest, lightPos) ); // this is how we're diong it with B&W illumination
+
+        //final illumination: direct light, shadows, and indirectLight approxomation
+        vec3 indirectLight = 0.5f*vec3( 1, 1, 1 );
+        PutPixelSDL(screen, i, j, triangles[closest.triangleIndex].color * (DirectLight(closest, lightPos) + indirectLight) ); // this is how we're diong it with B&W illumination
+
+
+
         // cout << "We painted a pixel?" << endl;
       }
       else{
@@ -231,8 +304,7 @@ void Draw(screen* screen, vec4 cameraPos, float yaw)
 }
 
 /*Place updates of parameters here*/
-bool Update(vec4& cameraPos, float& yaw)
-{
+bool Update(vec4& cameraPos, float& yaw, vec4& lightPos){
   static int t = SDL_GetTicks();
   /* Compute frame time */
   int t2 = SDL_GetTicks();
@@ -242,7 +314,7 @@ bool Update(vec4& cameraPos, float& yaw)
   std::cout << "Render time: " << dt << " ms." << std::endl;
   /* Update variables*/
   SDL_Event e;
-  bool lastWasRight = true;
+  // bool lastWasRight = true;
 
   while(SDL_PollEvent(&e))
     {
@@ -267,10 +339,10 @@ bool Update(vec4& cameraPos, float& yaw)
               case SDLK_LEFT:
               /* Move camera left */
                 // cameraPos.x -= .1; //first task
-                if(lastWasRight){
-                  yaw = yaw*-1;
-                  lastWasRight = false;
-                }
+                // if(lastWasRight){  //probably doens't work 18 Feb
+                //   yaw = yaw*-1;
+                //   lastWasRight = false;
+                // }
                 yaw += .01;
                 cameraPos.x = ( cameraPos.x*cos(yaw) - cameraPos.z*sin(yaw) );
                 cameraPos.z = ( cameraPos.z*cos(yaw) + cameraPos.z*sin(yaw) );
@@ -281,7 +353,31 @@ bool Update(vec4& cameraPos, float& yaw)
                 yaw -= .01;
                 cameraPos.x = ( cameraPos.x*cos(yaw) - cameraPos.z*sin(yaw) );
                 cameraPos.z = ( cameraPos.z*cos(yaw) + cameraPos.z*sin(yaw) );
-                lastWasRight = true;
+                // lastWasRight = true; //probably doens't work 18 Feb
+                break;
+              case SDLK_a:
+                /* Move light left */
+                lightPos.x -= .25;
+                break;
+              case SDLK_d:
+                /* Move light right */
+                lightPos.x += .25;
+                break;
+              case SDLK_w:
+                /* Move light forward */
+                lightPos.y -= .25;
+                break;
+              case SDLK_s:
+                /* Move light back */
+                lightPos.y += .25;
+                break;
+              case SDLK_q:
+                /* Move light up */
+                lightPos.z += .25;
+                break;
+              case SDLK_e:
+                /* Move light down */
+                lightPos.z -= .25;
                 break;
               case SDLK_ESCAPE:
               /* Move camera quit */
@@ -290,7 +386,7 @@ bool Update(vec4& cameraPos, float& yaw)
           }
     }
     //printing to see if the camera moving with keys works...
-    cout << "In update, (x: " << cameraPos.x << ", y: " << cameraPos.y << ", z: " << cameraPos.z << ")" << endl;
+    // cout << "In update, (x: " << cameraPos.x << ", y: " << cameraPos.y << ", z: " << cameraPos.z << ")" << endl;
     //end of moving camera testing
 
     return true;
